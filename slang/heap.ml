97f128dc55855d2df103d518331186fdd98c66e2
IntGrah <148660026+IntGrah@users.noreply.github.com>
2025-12-14 23:18:37 +0000

    Simplify heap implementation

--- slang/heap.ml
+++ slang/heap.ml
@@ -1,46 +1,36 @@
-(* Heap implementation shared by interpreters 0, 1, 2, 3 (Jargon VM uses an array) *)
-
 module Int_map = Map.Make (Int)
 
 type address = int
-(** Address is an alias for int, but this is hidden in the interface *)
+type 'a t = 'a Int_map.t
 
-type 'a t = { bindings : 'a Int_map.t; next : int }
-(** A heap is a map from integers to values, together with the next available
-    integer. *)
-
-let empty : 'a t = { bindings = Int_map.empty; next = 0 }
+let empty : 'a t = Int_map.empty
+let size : 'a t -> int = Int_map.cardinal
 
 let alloc (v : 'a) (h : 'a t) : address * 'a t =
-  if h.next < Option.heap_max then
-    (h.next, { bindings = Int_map.add h.next v h.bindings; next = h.next + 1 })
+  let next = size h in
+  if next < Option.heap_max then
+    (next, Int_map.add next v h)
   else
     Errors.complain "Runtime error: out of heap space"
 
 let get (a : address) (h : 'a t) : 'a =
-  match Int_map.find_opt a h.bindings with
+  match Int_map.find_opt a h with
   | None -> Errors.complain "Runtime error: invalid heap address"
   | Some v -> v
 
 let set (a : address) (v : 'a) (h : 'a t) : 'a t =
-  {
-    h with
-    bindings =
-      Int_map.update a
-        (function
-          | None -> Errors.complain "Runtime error: invalid heap address"
-          | Some _ -> Some v)
-        h.bindings;
-  }
+  Int_map.update a
+    (function
+      | None -> Errors.complain "Runtime error: invalid heap address"
+      | Some _ -> Some v)
+    h
 
-let to_list (h : 'a t) : (address * 'a) list = Int_map.bindings h.bindings
-let size (h : 'a t) : int = h.next
+let to_list : 'a t -> (address * 'a) list = Int_map.bindings
 let pp_binding pp_value ppf (a, v) = Format.fprintf ppf "%d -> %a" a pp_value v
 
 let pp pp_value ppf (h : 'a t) =
   Format.pp_print_list
     ~pp_sep:(fun ppf () -> Format.fprintf ppf "@;")
-    (pp_binding pp_value) ppf
-    (Int_map.bindings h.bindings)
+    (pp_binding pp_value) ppf (Int_map.bindings h)
 
 let pp_address = Format.pp_print_int
