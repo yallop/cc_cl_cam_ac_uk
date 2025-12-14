e3e249366779d40e51963e9adf5095ea97cded8d
IntGrah <148660026+IntGrah@users.noreply.github.com>
2025-12-14 20:51:43 +0000

    Reformat test and example files

--- slang/tests.ml
+++ slang/tests.ml
@@ -1,26 +1,50 @@
+let test_dir = "tests"
 
-let test_dir = "tests/" 
-
-let manifest_file = test_dir ^ "manifest.txt"
-
-let process_line file expected = 
-    Some(test_dir ^ file ^ ".slang", Some(expected))
-
-let scan_line in_chan = 
-    try
-       Scanf.bscanf in_chan "%s %s\n" process_line 
-    with 
-    | End_of_file -> None 
-    | ex -> let _ = print_string ("Unexpected Scanf exception : " ^ (Printexc.to_string ex) ^ "\n")
-            in None 
-
-let rec get_all in_chan = 
-    match (scan_line in_chan) with 
-    | None -> [] 
-    | Some(f, e)  -> (f, e) :: (get_all in_chan)
-
-let get_all_tests () = 
-    let in_chan = try 
-                    Scanf.Scanning.open_in manifest_file 
-                 with _ -> Errors.complain ("can't open file " ^ manifest_file)
-    in get_all in_chan 
+let manifest : (string * string option) list =
+  List.map
+    (fun (file, expected) -> (Filename.concat test_dir file, Some expected))
+    [
+      ("fib5_v2.slang", "8");
+      ("fib_rec_10.slang", "55");
+      ("fib_iter_42.slang", "267914296");
+      ("collatz_rec_27.slang", "111");
+      ("collatz_iter_27.slang", "111");
+      ("fact_rec_10.slang", "3628800");
+      ("fact_iter_10.slang", "3628800");
+      ("gcd_rec_105_75.slang", "15");
+      ("gcd_iter_105_75.slang", "15");
+      ("prime_rec_49.slang", "false");
+      ("prime_rec_101.slang", "true");
+      ("prime_iter_49.slang", "false");
+      ("prime_iter_101.slang", "true");
+      ("church.slang", "32");
+      ("poly_eq.slang", "42");
+      ("bool.slang", "false");
+      ("bidmas.slang", "4");
+      ("add3.slang", "101");
+      ("add4.slang", "101");
+      ("add5.slang", "101");
+      ("add6.slang", "101");
+      ("add7.slang", "101");
+      ("expr.slang", "101");
+      ("expr2.slang", "101");
+      ("let1.slang", "101");
+      ("nest.slang", "7");
+      ("nest2.slang", "42");
+      ("stack.slang", "32");
+      ("stack2.slang", "17");
+      ("alpha.slang", "72");
+      ("fun.slang", "101");
+      ("labels2.slang", "1");
+      ("labels3.slang", "6");
+      ("lambda1.slang", "101");
+      ("lambda2.slang", "101");
+      ("lambda3.slang", "101");
+      ("seq0.slang", "101");
+      ("seq1.slang", "101");
+      ("seq2.slang", "101");
+      ("ref1.slang", "101");
+      ("ref2.slang", "101");
+      ("ref3.slang", "101");
+      ("while.slang", "3");
+    ]
